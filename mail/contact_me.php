c7a8a5177d8739e94799e002abcf78070f37fc04
Tomás <tomas@comtom.com.ar>
2017-05-15 17:11:48 -0300

    version subida

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,12 +1,13 @@
 <?php
 // Check for empty fields
+
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Por favor, complete todos los campos.";
 	return false;
    }
 
@@ -15,12 +16,36 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-// Create the email and send the message
-$to = 'jerome.lachaud@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+require 'vendor/autoload.php';
+
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+ 
+$httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key' => '70c47502c2cab9fb073e66587098bb23899b2339']);
+ 
+$sparky->setOptions(['async' => false]);
+$results = $sparky->transmissions->post([
+  'options' => [
+    'sandbox' => false
+  ],
+  'content' => [
+    'from' => 'noreply@comtomtech.com',
+    'subject' => "[Comtom Tech] Contacto desde de la web: $name",
+    'html' => "<html><body><h2>You have received a new message from your website contact form.</h2>
+        <p>Here are the details:
+        <br>Name: $name
+        <br>Email: $email_address
+        <br>Phone: $phone
+        <br>Message:
+        <br>$message
+        </p></body></html>"
+  ],
+  'recipients' => [
+    ['address' => ['email'=>'ventas@comtomtech.com']]
+  ]
+]);
+
 return true;
 ?>
